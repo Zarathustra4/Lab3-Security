5e454474a7dfbd530b5c28b24fa9c32a73e5eeff
Max Voloshchuk <68013193+Zarathustra4@users.noreply.github.com>
2024-12-01 19:34:50 +0200

    Failure limit (#4)
    * tries
    
    * failure limit

--- app.py
+++ app.py
@@ -3,9 +3,15 @@ from flask_mail import Message, Mail
 from werkzeug.security import generate_password_hash, check_password_hash
 from itsdangerous import URLSafeTimedSerializer
 from dotenv import dotenv_values
+from datetime import timedelta, datetime
 
-from models import User, db
+from models import db, User, LoginAttempt
 from forms import RegistrationForm, LoginForm
+
+
+MAX_FAILED_ATTEMPTS = 5
+LOCK_TIME = timedelta(minutes=15)
+
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'
@@ -27,6 +33,16 @@ db.init_app(app)
 
 with app.app_context():
     db.create_all()
+    if User.query.filter_by(username="admin").first() is None:
+        admin = User(
+            email="admin@gmail.com",
+            username="admin",
+            password=generate_password_hash("admin"),
+            confirmed=True,
+            is_admin=True
+        )
+        db.session.add(admin)
+        db.session.commit()
 
 
 def generate_confirmation_token(email):
@@ -59,15 +75,31 @@ def login():
         username = form.username.data
         password = form.password.data
         user = User.query.filter_by(username=username).first()
-        if user and check_password_hash(user.password, password):
-            if user.confirmed:
-                session['user_id'] = user.id
-                flash('Login successful!', 'success')
-                return redirect(url_for('account'))
+
+        login_attempt = LoginAttempt(username=username, success=False, timestamp=datetime.now())
+        db.session.add(login_attempt)
+
+        if user:
+            if check_password_hash(user.password, password):
+                if user.confirmed:
+                    login_attempt.success = True
+                    user.failed_attempts = 0
+                    session['user_id'] = user.id
+                    flash('Login successful!', 'success')
+                    db.session.commit()
+                    return redirect(url_for('account'))
+                else:
+                    flash('Please confirm your email to activate your account.', 'warning')
             else:
-                flash('Please confirm your email to activate your account.', 'warning')
+                user.failed_attempts += 1
+                user.last_failed_attempt = datetime.now()
+                db.session.commit()
+                flash('Invalid username or password', 'danger')
         else:
             flash('Invalid username or password', 'danger')
+        
+        db.session.commit()
+        
     return render_template('login.html', form=form)
 
 
@@ -81,8 +113,8 @@ def registration():
 
         if User.query.filter_by(username=username).first():
             flash('Username already exists', 'danger')
-        # if User.query.filter_by(email=email).first():
-        #     flash("Email already exists", 'danger')
+        if User.query.filter_by(email=email).first():
+            flash("Email already exists", 'danger')
         else:
             hashed_password = generate_password_hash(password)
             new_user = User(username=username,
@@ -130,7 +162,8 @@ def account():
         return redirect(url_for('login'))
 
     user = User.query.get(session['user_id'])
-    return render_template('account.html', user=user)
+    attempts = LoginAttempt.query.order_by(LoginAttempt.timestamp.desc()).all()
+    return render_template('account.html', user=user, attempts=attempts)
 
 
 @app.route('/logout')
